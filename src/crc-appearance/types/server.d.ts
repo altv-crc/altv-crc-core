37c404b71f4d826de35ff3f27beaf699bdabe507
Stuyk <stuykgaming@gmail.com>
2023-07-12 17:10:28 -0600

    Delete src/crc-appearance directory

--- src/crc-appearance/types/server.d.ts
+++
@@ -1,8 +0,0 @@
-import * as alt from 'alt-server';
-import { Appearance } from '../shared/index';
-
-declare module 'alt-server' {
-    interface ICustomEmitEvent {
-        'crc-appearance-apply': (player: alt.Player, data: Appearance) => void;
-    }
-}
